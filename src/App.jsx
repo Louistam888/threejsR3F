0f50d0f6ac8c87b6f9cbabad7b104336d116d7ca
Louistam888 <louistam888@gmail.com>
2026-04-22 21:33:48 -0400

    added orbit controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import { div } from "three/tsl";
+import { useState, useRef, useEffect } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  FirstPersonControls,
+  OrbitControls,
+  GizmoHelper,
+  GizmoViewcube,
+  GizmoViewport,
+} from "@react-three/drei";
+
+function AnimatedBox() {
+  const boxRef = useRef();
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.005;
+    boxRef.current.rotation.y += 0.005;
+    boxRef.current.rotation.z += 0.005;
+  });
+
+  return (
+    <mesh ref={boxRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <axesHelper args={[10]} />
+      <meshToonMaterial color={0x00bfff} />
+    </mesh>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,21 +31,21 @@ function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <mesh
-          position={[-2, 2, -1]}
-          rotation={[0, 0, Math.PI]}
-          scale={[2, 0.5, 2]}
-        >
-          {/* <sphereGeometry args={[3, 80, 80]} /> */}
-          {/* <boxGeometry args={[2, 3, 2]} /> */}
-          <torusKnotGeometry args={[1.7, 0.3, 256, 256]} />
-          <meshToonMaterial color={0x00bfff} />
-          <directionalLight position={[2, 5, 1]} />
-        </mesh>
+        <GizmoHelper alignment="bottom-right" margi={[80, 80]}>
+          <GizmoViewcube />
+        </GizmoHelper>
+
+        <gridHelper args={[20, 20, 0xff22aa, 0x55cff]} />
+        <axesHelper args={[10]} />
+        <OrbitControls />
+        <FirstPersonControls movementSpeed={1} />
+        <AnimatedBox />
+        <directionalLight position={[2, 5, 1]} />
       </Canvas>
     </div>
   );
 }
 
 export default App;
-//8:25
+
+//1405
